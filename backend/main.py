4bad49864d1d37fef9ebcbe7a8eb213cca15a95d
lasan <limahaliyana@gmail.com>
2024-08-25 20:59:40 +0530

    added backend logic for starting bot

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,60 @@ from fastapi import FastAPI
 import uvicorn
 from os import environ
 app = FastAPI()
+from pydantic import BaseModel
+import requests
+
+metadataUrl = "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token"
+zoombotTestingStartUrl = "https://run.googleapis.com/v2/projects/woven-arcadia-432212-a6/locations/us-central1/jobs/zoombot-testing:run"
+
+class MeetingRequest(BaseModel):
+    meetingUrl: str
+    timeout: int
+    botName: str
+    botId: str
+    fromId: str
+    wsLink: str
 
 
 @app.get("/", tags=["root"])
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/test/zoom")
+async def launch_zoombot(request:MeetingRequest):
+    payload = {
+        'overrides':{
+            "containerOverrides":[
+                {
+                    "env":[
+                        {"name":"MEETING_URL","value":request.meetingUrl},
+                        {"name":"BOTNAME","value":request.botName},
+                        {"name":"TIMEOUT","value":request.timeout},
+                        {"name":"BOT_ID","value":request.botId},
+                        {"name":"WS_LINK","value":request.wsLink},
+                        {"name":"FROM_ID","value":request.fromId},
+                    ]
+                }
+            ]
+        }
+    }
+    r = requests.post(
+        metadataUrl,
+        headers={
+            "Metadata-Flavor":"Google"
+        }
+    )
+    access_token = r.json()['access_token']
+    start_job = requests.post(
+        zoombotTestingStartUrl,
+        headers={
+            'Authorization':f'Bearer {access_token}',
+            "Content-Type":'application/json'
+        },
+        data=payload
+    )
+    return start_job.json()
+     
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=int(environ.get('PORT')))
+    uvicorn.run(app, host='0.0.0.0', port=int(environ.get('PORT') or 8000))
