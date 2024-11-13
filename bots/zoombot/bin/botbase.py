69a759f790ccbd4d5ccd14d7067bd866ea0306e3
lasan <limahaliyana@gmail.com>
2024-11-13 23:01:50 +0530

    Added use fake ui for media stream instead of setting prefs

--- bots/zoombot/bin/botbase.py
+++ bots/zoombot/bin/botbase.py
@@ -36,10 +36,9 @@ class BotBase:
         opt.add_argument('--start-maximized')
         opt.add_argument('--headless')
         opt.add_argument('--use-fake-device-for-media-stream')
+        opt.add_argument('--use-fake-ui-for-media-stream')
 
         opt.add_experimental_option("prefs", {
-            "profile.default_content_setting_values.media_stream_mic": 1,
-            "profile.default_content_setting_values.media_stream_camera": 1,
             "profile.default_content_setting_values.geolocation": 0,
             "profile.default_content_setting_values.notifications": 1
         })
